a1bc80ae89b4387eff874fd102f84c1411cdde21
GermanGarzonFET <german_garzonto@fet.edu.do>
2020-02-27 22:29:23 -0500

    Update inicio.blade.php

--- resources/views/inicio/inicio.blade.php
+++ resources/views/inicio/inicio.blade.php
@@ -6,96 +6,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <title>inicio</title>
+    <title>Generos</title>
   </head>
   <body>
-<!-- inicia el codigo de login -->
-<ul class="nav">
-    <li class="nav-item">
-    <a class="nav-link" href="{{ 'inicio' }}">inicio</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="{{ 'inventario' }}">inventario</a>
-    </li>
-  </ul>
-<br>
-
-<div class="container">
-    <h1>listas libros</h1>
-<!-- boten de Modal -->
-
-<!-- listar -->
-
-@foreach ($inventario as $lis)
-
-<div class="card">
-    <div class="card-header">
-      <h3>{{$lis->nombre}}</h3>
-    </div>
-    <div class="card-body">
-    <h5><strong>Descripcion</strong></h5>
-    <p class="card-text">{{$lis->descripcion}}</p>
-    <h4>Autor: {{$lis->autor}}</h4>
-    <h3>{{$lis->precio}}</h3>
-      <a href="#" class="btn btn-primary">acomprar</a>
-    </div>
-  </div>
-  <br>
-  @endforeach
-
-<!-- tarjetas 
-
-
-
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-
-                    <table class="table">
-                        <thead>
-                          <tr>
-                            <div class="col-1"></div>
-                            <th scope="col-1">Id</th>
-                            <th scope="col-2">Nombre</th>
-                            <th scope="col-2">Descripcion</th>
-                            <th scope="col-2">N*pagina</th>
-                            <th scope="col-1">Edicion</th>
-                            <th scope="col-1">Autor</th>
-                            <th scope="col-1">Precio</th>
-                            <th scope="col-1">opciones</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          @foreach ($inventario as $lis)
-                          <tr>
-                          <th scope="row">{{$lis->id_libro}}</th>
-                            <td>{{$lis->nombre}}</td>
-                            <td>{{$lis->descripcion}}</td>
-                            <td>{{$lis->npagina}}</td>
-                            <td>{{$lis->edicion}}</td>
-                            <td>{{$lis->autor}}</td>
-                            <td>{{$lis->precio}}</td>
-                          </tr>
-                          @endforeach
-                        </tbody>
-                      </table>
-
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Libreria</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="..." id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Generos
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="/novela">Novela</a>
+              <a class="dropdown-item" href="{{'/libros/ficcion'}}">Ficción</a>
+                <a class="dropdown-item" href="#">Satira</a>
+                <a class="dropdown-item" href="#">Humor</a>
+              <a class="dropdown-item" href="{{'/libors/novela'}}">Literatura</a>
+                <a class="dropdown-item" href="#">Thriller</a>
+                <a class="dropdown-item" href="#">Horror</a>
+                <a class="dropdown-item" href="#">Misterio</a>
+                <a class="dropdown-item" href="#">Suspenso</a>
+              </div>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Categorias
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Ciencias</a>
+                  <a class="dropdown-item" href="#">arte</a>
+                  <a class="dropdown-item" href="#">Historia</a>
+                  <a class="dropdown-item" href="#">Infantiles</a>
+                  <a class="dropdown-item" href="#">Novelas y literatura</a>
+                  <a class="dropdown-item" href="#">Juvenil y ficcion</a>
                 </div>
-            </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Login</a>
+            </li>
+          </ul>
         </div>
-    </div>
-</div>
--->
-
-
+      </nav>
+    <h2>bibliotea online</h2>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
 </html>
