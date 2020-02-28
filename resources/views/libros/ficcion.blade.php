cdbf0d958d220aef8f98447eb410e5fb8a224bd4
GermanGarzonFET <german_garzonto@fet.edu.do>
2020-02-27 22:29:25 -0500

    Create ficcion.blade.php

---
+++ resources/views/libros/ficcion.blade.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Novelas</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Libreria</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Generos
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="/novela">Novela</a>
+                <a class="dropdown-item" href="/ficcion">Ficción</a>
+                <a class="dropdown-item" href="#">Satira</a>
+                <a class="dropdown-item" href="#">Humor</a>
+                <a class="dropdown-item" href="#">Literatura</a>
+                <a class="dropdown-item" href="#">Thriller</a>
+                <a class="dropdown-item" href="#">Horror</a>
+                <a class="dropdown-item" href="#">Misterio</a>
+                <a class="dropdown-item" href="#">Suspenso</a>
+              </div>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Categorias
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Ciencias</a>
+                  <a class="dropdown-item" href="#">arte</a>
+                  <a class="dropdown-item" href="#">Historia</a>
+                  <a class="dropdown-item" href="#">Infantiles</a>
+                  <a class="dropdown-item" href="/novelaliteratura">Novelas y literatura</a>
+                  <a class="dropdown-item" href="#">Juvenil y ficcion</a>
+                </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Login</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <h3>Ficción</h3>
+
+      
+      <div class="card-deck">
+        <div class="card">
+          <img src="https://libreriadelau.vteximg.com.br/arquivos/ids/2048693-1000-1000/La-vuelta-al-mundo-en-80-dias-9788417079307-prom.jpg?v=636468865873530000" class="card-img-top" alt="">
+          <div class="card-body">
+            <h5 class="card-title">La vuelta al mundo en 80 dias</h5>
+            <p class="card-text">La vuelta al mundo en ochenta días es una novela del escritor francés Julio Verne publicada por entregas en Le Temps desde el 7 de noviembre hasta el 22 de diciembre de 1872, el mismo año en que se sitúa la acción. Después, sería publicada de manera íntegra el 30 de enero de 1873.</p>
+            <p class="card-text"><small class="text-muted">Libro de texto, literatura fantástica, ficción de aventuras</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img src="https://librerialerner.vteximg.com.br/arquivos/ids/303635-1000-1000/Lerner-733.jpg?v=637006366953300000" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Las minas del rey salomon</h5>
+            <p class="card-text">Las minas del rey Salomón es una popular novela del escritor victoriano de aventuras y fábulas, H. Rider Haggard, escrita en 1882</p>
+            <p class="card-text"><small class="text-muted">Novela, mundo perdido, ficción de aventuras</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img src="https://librerialerner.vteximg.com.br/arquivos/ids/253583-1000-1000/IJ00044001.jpg?v=636482667151000000" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">La flecha negra</h5>
+            <p class="card-text">La flecha negra	Novela, Novela rosa, ficción historica	Robert Louis Stevenson	La flecha negra es una novela histórica escrita por Robert Louis Stevenson en 1888. Su título original en inglés es The Black Arrow: A Tale of the Two Roses. Su primera publicación fue en formato de folleto y con el pseudónimo de capitán George North.</p>
+            <p class="card-text"><small class="text-muted">Novela, Novela rosa, ficción historica</small></p>
+          </div>
+        </div>
+      </div>
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
