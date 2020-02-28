d5fd82227b4ac5b06e6b89cdff3fc3d5ee1eeb7d
GermanGarzonFET <german_garzonto@fet.edu.do>
2020-02-28 08:48:04 -0500

    Update inventario.blade.php

--- resources/views/inventario/inventario.blade.php
+++ resources/views/inventario/inventario.blade.php
@@ -1,27 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <title>inicio</title>
-  </head>
-  <body>
-<!-- inicia el codigo de login -->
-<ul class="nav">
-    <li class="nav-item">
-    <a class="nav-link" href="{{ 'inicio' }}">inicio</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="{{ 'inventario' }}">inventario</a>
-    </li>
-  </ul>
-<br>
-
+@extends('layout')
+@section('content')
 <div class="container">
     <h1>inventario libros</h1>
 <!-- boten de Modal -->
@@ -43,8 +22,7 @@
                             <th scope="col-1">Id</th>
                             <th scope="col-2">Nombre</th>
                             <th scope="col-2">Descripcion</th>
-                            <th scope="col-2">N*pagina</th>
-                            <th scope="col-1">Edicion</th>
+                            <th scope="col-2">Genero</th>
                             <th scope="col-1">Autor</th>
                             <th scope="col-1">Precio</th>
                             <th scope="col-1">opciones</th>
@@ -56,8 +34,7 @@
                           <th scope="row">{{$inven->id_libro}}</th>
                             <td>{{$inven->nombre}}</td>
                             <td>{{$inven->descripcion}}</td>
-                            <td>{{$inven->npagina}}</td>
-                            <td>{{$inven->edicion}}</td>
+                            <td>{{$inven->genero}}</td>
                             <td>{{$inven->autor}}</td>
                             <td>{{$inven->precio}}</td>
                             <td>  
@@ -174,10 +151,4 @@
   </div>
 </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
+@endsection
